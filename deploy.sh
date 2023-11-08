09ead315cb8ba48637dec0ef84e56a8103f28f0b
sayeda-77 <142560608+sayeda-77@users.noreply.github.com>
2023-11-08 10:58:34 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,12 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Log in to Docker Hub (replace with your actual Docker Hub credentials)
     docker login -u sayeda77 -p dckr_pat_VPY-9NwBDEwI0PpfGcF3l8ujH28
 
-    # Build the Docker image (replace "image" with the actual image name)
-    docker build -t react-appimg .
-
+  
     # Tag the image
-    docker tag react-appimg dev/dockerhub/repository:latest
+    docker tag react-appimg sayeda77/dev
 
     # Push the image to the Dev Docker Hub repository
-    docker push dev/dockerhub/repository:latest
+    docker push sayeda77/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     # Build your project
@@ -24,16 +22,12 @@ elif [[ $GIT_BRANCH == "origin/main" ]]; then
     # Log in to Docker Hub (replace with your actual Docker Hub credentials)
     docker login -u sayeda77 -p dckr_pat_VPY-9NwBDEwI0PpfGcF3l8ujH28
 
-    # Build the Docker image (replace "image" with the actual image name)
-    docker build -t react-appimg .
-
+   
     # Tag the image
-    docker tag react-appimg prod/dockerhub/repository:latest
+    docker tag react-appimg sayeda77/prod 
 
     # Push the image to the Prod Docker Hub repository
-    docker push prod/dockerhub/repository:latest
-
+    docker push sayeda77/prod
 else
     echo "Deployment error"
 fi
-
